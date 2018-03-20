b9f3703f20cdff78768d2578781ededfd09a3738
troykuo <troykuo.gzx@gmail.com>
2018-03-21 00:43:34 +1100

    commit before pull. And almost finish task4

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -3,13 +3,23 @@ package comp1110.ass2.gui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
 
 /**
  * A very simple viewer for card layouts in the Warring States game.
@@ -26,15 +36,62 @@ public class Viewer extends Application {
 
     private final Group root = new Group();
     private final Group controls = new Group();
+    private final Group grid=new Group();
     TextField textField;
+
+
 
     /**
      * Draw a placement in the window, removing any previously drawn one
      *
      * @param placement A valid placement string
      */
-    void makePlacement(String placement) {
+
+     void makePlacement(String placement) {
         // FIXME Task 4: implement the simple placement viewer
+
+
+        int listLength=placement.length()/3;
+        String [] orientationList=new String [listLength];
+        String [] cardList=new String[listLength];
+
+        for(int i=0; i<listLength;++i)
+        {
+            for (int j=0; j<placement.length();j=j+3)
+            {
+                cardList[i]=Character.toString(placement.charAt(j))+Character.toString(placement.charAt(j+1));
+                orientationList[i]=Character.toString(placement.charAt(i+2));
+            }
+
+        }
+
+//         GridPane gridPane=new GridPane();
+//
+//         gridPane.setAlignment(Pos.CENTER);
+//         gridPane.setHgap(10);
+//         gridPane.setVgap(10);
+//         gridPane.setPadding(new Insets(8,8,8,8));
+//
+//         final int column=6;
+//         final int row=6;
+//         int tempColumn=0;
+//         int tempRow=0;
+//         Button button[][]=new Button[column][row];
+//
+//         for( int i=0; i<column; i++)
+//         {
+//             for(int j=0; j<row; j++)
+//             {
+//                 button[i][j]=new Button("");
+//                 button[i][j].setPrefSize(95,95);
+//                 gridPane.add(button[i][j],i,j);
+//             }
+//         }
+//        grid.getChildren().add(gridPane);
+
+//         System.out.println("1"+orientationList[0]);
+//         System.out.println("2"+cardList[0]);
+
     }
 
     /**
@@ -48,16 +105,51 @@ public class Viewer extends Application {
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
+                System.out.println(textField.getText());
                 makePlacement(textField.getText());
                 textField.clear();
+
             }
         });
+
+        GridPane gridPane=new GridPane();
+
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(8,8,8,8));
+
+        final int column=6;
+        final int row=6;
+        int tempColumn=0;
+        int tempRow=0;
+        Button button1[][]=new Button[column][row];
+
+        for( int i=0; i<column; i++)
+        {
+            for(int j=0; j<row; j++)
+            {
+                button1[i][j]=new Button("");
+                button1[i][j].setPrefSize(95,95);
+                gridPane.add(button1[i][j],i,j);
+            }
+        }
+//        grid.getChildren().add(gridPane);
+
+        HBox hb1=new HBox();
+        hb1.getChildren().add(gridPane);
+
+
+
+
         HBox hb = new HBox();
         hb.getChildren().addAll(label1, textField, button);
         hb.setSpacing(10);
         hb.setLayoutX(130);
         hb.setLayoutY(VIEWER_HEIGHT - 50);
-        controls.getChildren().add(hb);
+//        hb.setLayoutY(0);
+        controls.getChildren().addAll(hb,hb1);
+
     }
 
     @Override
@@ -65,9 +157,46 @@ public class Viewer extends Application {
         primaryStage.setTitle("Warring States Viewer");
         Scene scene = new Scene(root, VIEWER_WIDTH, VIEWER_HEIGHT);
 
+
+
         root.getChildren().add(controls);
+//        root.getChildren().add(grid);
 
         makeControls();
+
+//        System.out.println("1"+orientationList[0]);
+//        System.out.println("2"+cardList[0]);
+//        GridPane gridPane=new GridPane();
+//
+//        gridPane.setAlignment(Pos.CENTER);
+//        gridPane.setHgap(10);
+//        gridPane.setVgap(10);
+//        gridPane.setPadding(new Insets(8,8,8,8));
+//
+//        final int column=6;
+//        final int row=6;
+//        int tempColumn=0;
+//        int tempRow=0;
+//        Button button[][]=new Button[column][row];
+//
+//        for( int i=0; i<column; i++)
+//        {
+//            for(int j=0; j<row; j++)
+//            {
+//                button[i][j]=new Button("");
+//                button[i][j].setPrefSize(95,95);
+//                gridPane.add(button[i][j],i,j);
+//            }
+//        }
+
+
+
+
+
+//        root.getChildren().add(gridPane);
+
+
+
 
         primaryStage.setScene(scene);
         primaryStage.show();
